2d57c72c92186abf4d411f06536f657bb0706c27
Heresleo <itsmegautam10@outlook.com>
2019-03-04 20:50:04 +0530

    Points.
    Coin animation and coin collection.

---
+++ DissSunny/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMaster : MonoBehaviour
+{
+    public int points;
+
+    public Text pointText;
+
+    void Update()
+    {
+        pointText.text = ("Points: " + points);
+    }
+}
---
+++ DissSunny/Assets/coinC.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinC : MonoBehaviour
+{
+    private GameMaster gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<GameMaster>();
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Coin"))
+        {
+            Destroy(col.gameObject);
+            gm.points += 1;
+        }
+    }
+  
+}
