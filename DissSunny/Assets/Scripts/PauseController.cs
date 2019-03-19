5d002175b7b06237d850381e1b42827387229085
Heresleo <itsmegautam10@outlook.com>
2019-03-19 17:36:58 +0530

    Final.

---
+++ DissSunny/Assets/Scripts/PauseController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject PauseMenu;
+    
+    public void ResumeGame()
+    {
+        PauseMenu.SetActive(false);
+    }
+    public void PauseGame()
+    {
+        PauseMenu.SetActive(true);
+    }
+}
